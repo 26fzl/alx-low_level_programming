6c0ea0419b704097702157f11b859cfb9773969f
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-04-25 11:24:40 -0700

    2 print alphabet x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,11 +10,11 @@ void print_alphabet_x10(void)
 
 	while (i <= 9)
 	{
-		for (letter = "a"; letter <= "z"; letter++)
-		{
-			_putchar(letter);
-		}
-		_putchar('\n');
-		i++;
+	for (letter = "a"; letter <= "z"; letter++)
+	{
+	_putchar(letter);
+	}
+	_putchar('\n');
+	i++;
 	}
 }
