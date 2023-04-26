7aee161ede521170dad8b945067e66af69c9e39c
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-04-26 10:50:09 -0700

    102 fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,19 +6,19 @@
 int main(void)
 {
 	int i = 0;
-	int j = 1, k = 2, temp;
+	long j = 1, k = 2, ;
 
 	while (i < 50)
 	{
 		if (i == 0)
-			printf("%d", j);
+			printf("%ld", j);
 		else if (i == 1)
-			printf(", %d", k);
+			printf(", %ld", k);
 		else
 		{
 			k += j;
 			j = k - j;
-			printf(", %d", k);
+			printf(", %ld", k);
 		}
 		i++;
 	}
