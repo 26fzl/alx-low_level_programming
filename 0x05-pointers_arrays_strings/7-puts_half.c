19092e102d3059ad0529ec723e063d1b58103a9c
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-05-02 09:30:15 -0700

    7 puts half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: the string to be printed
+ * Return: nothing
+ */
+void puts_half(char *str)
+{
+	int e;
+
+	for (e = 0; str[e] != '\0'; e++)
+	{
+		e++;
+	}
+	for (e /= 2; str[e] != '0'; e++)
+	{
+		_putchar(str[e]);
+	}
+	_putchar('\n');
+}
