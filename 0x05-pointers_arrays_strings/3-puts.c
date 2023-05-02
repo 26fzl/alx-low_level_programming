4bddd07f6a4a4b335b0eb27df314c6af33f37b6b
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-05-02 07:30:16 -0700

    3 puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts -print a string to stdout
+ * @str: the string to print
+ * Return: the lenght of string
+ *
+ */
+void _puts(char *str)
+{
+	while (*str != '\0'; str++)
+	{
+	_putchar(*str);
+	}
+	_putchar('\n');
+}
