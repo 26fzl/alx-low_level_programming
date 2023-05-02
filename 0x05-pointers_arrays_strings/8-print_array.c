e14bcbbf10dd6aa57ddd4760f5476310601c66da
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-05-02 09:59:01 -0700

    8 print array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,14 +10,14 @@ void print_array(int *a, int n)
 {
 	int x;
 
-	for (x = 0; x < n ; x++)
+	for (x = 0; x < n; x++)
 	{
 		if (x != (n - 1))
 			printf("%d, ", a[x]);
 		else
 			printf("%d", a[x]);
 	}
-	printf('\n');
+	printf("\n");
 }
 
 
