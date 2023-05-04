496e45b7d3969243a0892ec533fc351c838181d2
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-05-04 00:39:14 -0700

    0 strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * *_strcat -  concatenates two strings
+ * @src: input string
+ * @dest: input string
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int x;
+	int y;
+
+	x = 0;
+	for (dest[x] != '\0')
+	{
+		x++;
+	}
+	y = 0;
+	for (src[y] != '\0')
+	{
+		dest[x] = src[y];
+		x++;
+		y++;
+	}
+	dest[x] = '\0';
+	return (dest);
+}
