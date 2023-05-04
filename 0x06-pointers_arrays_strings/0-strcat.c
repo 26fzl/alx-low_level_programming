adeb17b80e3ae941c2e4d06cade962c6e70306c4
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-05-04 00:55:22 -0700

    0 strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,12 +11,12 @@ char *_strcat(char *dest, char *src)
 	int y;
 
 	x = 0;
-	for (dest[x] != '\0')
+	while (dest[x] != '\0')
 	{
 		x++;
 	}
 	y = 0;
-	for (src[y] != '\0')
+	while (src[y] != '\0')
 	{
 		dest[x] = src[y];
 		x++;
