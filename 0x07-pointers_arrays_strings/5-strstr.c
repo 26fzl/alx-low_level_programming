a673588f45477648ce8beb6ee860c228c482ae97
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-05-08 04:20:38 -0700

    5 strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strstr - locate a substring
+ * @haystack: input string
+ * @needle: input substring
+ * Return: 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+		{
+			if (*haystack == *needle)
+			{
+				char *h = haystack;
+				char *e = needle;
+
+				while (*h == *e && *e != '\0')
+				{
+					h++;
+					e++;
+				}
+
+			if (*e == '\0')
+			{
+				return (haystack);
+			}
+			}
+		}
+	return (0);
+}
