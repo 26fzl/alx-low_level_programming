e08ac31a27a4e7ea59a2c3370a4257caedadd9c3
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-05-08 05:46:27 -0700

    7 print chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_chessboard - prints the chessboard.
+ * @a: array
+ * Return: 0
+ */
+void print_chessboard(char (*a)[8])
+{
+	int u;
+	int v;
+
+	for (u = 0; u < 8; u++)
+	{
+		for (v = 0; v < 8; v++)
+			_putchar(a[u][v]);
+		_putchar('\n');
+	}
+}
