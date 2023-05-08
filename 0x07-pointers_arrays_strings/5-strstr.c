89f7f015637a84d07b3d147131368c5734be28c7
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-05-08 05:38:52 -0700

    5 strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,21 +10,18 @@ char *_strstr(char *haystack, char *needle)
 {
 	for (; *haystack != '\0'; haystack++)
 	{
-		if (*haystack == *needle)
-		{
-			char *h = haystack;
-			char *e = needle;
+		char *h = haystack;
+		char *e = needle;
 
-			while (*h == *e && *e != '\0')
-			{
-				h++;
-				e++;
-			}
+		while (*h == *e && *e != '\0')
+		{
+			h++;
+			e++;
+		}
 
 		if (*e == '\0')
 		{
 			return (haystack);
-		}
 		}
 	}
 	return (NULL);
