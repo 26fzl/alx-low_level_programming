d510fef9bf8a17c83375f1869d4cea93d5335f4d
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-05-10 02:51:02 -0700

    2 strlen recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion - Return the length of a string.
+ * @s: The string
+ * Return: The length of s.
+ */
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s)
+	{
+		len++;
+		len += _strlen_recursion(s + 1);
+	}
+
+	return (len);
+}
