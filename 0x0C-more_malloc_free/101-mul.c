7c8a63dcc3cb276ef68020b657fe258b665504b7
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-05-19 13:23:04 -0700

    101 mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,72 +2,102 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int find_len(char *str);
+int is_digit(char *s);
+int _strlen(char *s);
+void errors(void);
 
 
-void add_nums(char *Finai_prod, char *next_prod, int next_len);
+
+
 /**
- * find_len - Finds the length of a string.
- * @str: The string
- * Return: length of the string.
+ * is_digit - check if the string have a non-digit char
+ * @s: the string to be evaluated
+ * Return: 0 if there is a non-digit, 1 otherwise
  */
-int find_len(char *str)
+int is_digit(char *s)
 {
-	int l = 0;
+	int w = 0;
 
-	while (*str++)
-		l++;
-	return (l);
+	while (s[w])
+	{
+		if (s[w] < '0' || s[w] > '9')
+			return (0);
+		w++;
+	}
+	return (1);
 }
-char *create_xarray(int size);
 
 /**
- * create_xarray - create array of char
- * @size: the size of the array
- * Return: a pointer to the array
+ * _strlen - return the length of a string
+ * @s: the string to be  evaluated
+ * Return: the length of the string
  */
-char *create_xarray(int size)
+int _strlen(char *s)
 {
-	char *arr;
-	int idx;
+	int w = 0;
 
-	arr = malloc(sizeof(char) * size);
+	while (s[w] != '\0')
+	{
+		w++;
+	}
+	return (w);
+}
 
-	if (arr == NULL)
+/**
+ * errors - correct errors for main
+ */
+void errors(void)
+{
+	printf("Error\n");
 	exit(98);
-
-	for (idx = 0; idx < (size - 1); idx++)
-		arr[idx] = 'x';
-	arr[idx] = '\0';
-	return (arr);
-}
-char *iterate_zeroes(char *str);
-/**
- * iterate_zeroes - itereate through a string of numbers
- * @str: the string
- * Return: pointer to the non zero element
- */
-char *iterate_zeroes(char *str)
-{
-	while (*str && *str == '0')
-		str++;
-	return(str);
 }
 
-void get_prod(char prod, char *mult, int digit, int zeroes);
 /**
- * get_prod - multiplies numbers
- * @prod: buffer to store results
- * @digit: single digit
- * @mult: string
- * @zeroes: number
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: always 0
  */
-void get_prod(char prod, char *mult, int digit, int zeroes)
+int main(int argc, char *argv[])
 {
-	int m, n, t = 0;
+	char *s1, *s2;
+	int l1, l2, l, w, c, d1, d2, *rslt, x = 0;
 
-	m = find_len(mult) - 1;
-	mult += m;
-
-	while 
-
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	l1 = _strlen(s1);
+	l2 = _strlen(s2);
+	l = l1 + l2 + 1;
+	rslt = malloc(sizeof(int) * l);
+	if (!rslt)
+		return (1);
+	for (w = 0; w <= l1 + l2; w++)
+		rslt[w] = 0;
+	for (l1 = l1 - 1; l1 >= 0; l1--)
+	{
+		d1 = s1[l1] - '0';
+		c = 0;
+		for (l2 = _strlen(s2) - 1; l2 >= 0; l2--)
+		{
+			d2 = s2[l2] - '0';
+			c += rslt[l1 + l2 + 1] + (d1 * d2);
+			rslt[l1 + l2 + 1] = c % 10;
+			c /= 10;
+		}
+		if (c > 0)
+			rslt[l1 + l2 + 1] += c;
+	}
+	for (w = 0; w < l - 1; w++)
+	{
+		if (rslt[w])
+			x = 1;
+		if (x)
+			_putchar(rslt[w] + '0');
+	}
+	if (!x)
+		_putchar('0');
+	_putchar('\n');
+	free(rslt);
+	return (0);
+}
