61037c1e5deb35538b80b87f7e62af7c2c0bc2b7
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-05-24 06:33:18 -0700

    task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,42 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+/**
+ * main - check arguments
+ * @argc: arg 1
+ * @argv: arg 2
+ * Return: error
+ */
+int main(int argc, char *argv[])
+{
+	int x, y;
+	char *op;
+
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	x = atoi(argv[1]);
+	op = argv[2];
+	y = atoi(argv[3]);
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*op == '/' && y == 0) ||
+	    (*op == '%' && y == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(op)(x, y));
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+/**
+ * op_add - Return the sum of two numbers.
+ * @a: The first one.
+ * @b: The second one.
+ * Return: The sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Return the difference of two numbers.
+ * @a: The first one
+ * @b: The second one
+ * Return: The difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Returns the product of two numbers.
+ * @a: The first one.
+ * @b: The second one
+ * Return: The product.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Return the division of two numbers.
+ * @a: The first one
+ * @b: The second one
+ * Return: The quotient
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Return the remainder of the division of two numbers.
+ * @a: The first one
+ * @b: The second one
+ * Return: The remainder of the division.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
