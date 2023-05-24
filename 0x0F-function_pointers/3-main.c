77219e46dc4a75e86236a9f516e3f888da1d55eb
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-05-24 09:43:59 -0700

    task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -4,8 +4,8 @@
 #include "3-calc.h"
 /**
  * main - check arguments
- * @argc: arg 1
- * @argv: arg 2
+ * @argc: first arg
+ * @argv: second arg
  * Return: always 0
  */
 int main(int argc, char **argv)
@@ -18,19 +18,25 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(98);
 	}
+
 	op = get_op_func(argv[2]);
+
 	if (!op)
 	{
 		printf("Error\n");
 		exit(99);
 	}
+
 	num2 = atoi(argv[3]);
+
 	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
+
 	num1 = atoi(argv[1]);
+
 	printf("%d\n", op(num1, num2));
 
 	return (0);
