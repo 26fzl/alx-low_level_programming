5e9b15be9148e7ddcd99b62b9eaf03d21b5e6674
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-05-25 03:47:30 -0700

    0 sum them all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -5,7 +5,7 @@
  * sum_them_all - Return the sum of all its paramters.
  * @n: number of paramters
  * @...: A variable number of paramters to calculate the sum of.
- * Return: 0 if n == 0 or the sum of all parameters.
+ * Return: 0 otherwise sum.
  */
 int sum_them_all(const unsigned int n, ...)
 {
@@ -18,9 +18,9 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(arg, n);
 
 	for (i = 0; i < n; i++)
-
+	{
 		sum += va_arg(arg, int);
-
+	}
 	va_end(arg);
 
 	return (sum);
