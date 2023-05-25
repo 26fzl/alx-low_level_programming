18706fac140194f98f75473f1be503bcc0f6d238
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-05-25 03:40:40 -0700

    0 sum them all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,17 +9,19 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list a;
+	va_list arg;
 	unsigned int i, sum = 0;
 
-	va_start(a, n);
+	if (n == 0)
+		return (0);
+
+	va_start(arg, n);
 
 	for (i = 0; i < n; i++)
-	{
-		sum += va_arg(a, int);
-	}
 
-	va_end(a);
+		sum += va_arg(arg, int);
+
+	va_end(arg);
 
 	return (sum);
 }
