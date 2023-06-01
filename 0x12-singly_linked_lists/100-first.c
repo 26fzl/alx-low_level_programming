1ca14839818195aaaf3d271ab33334a5b4ddbdce
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-06-01 04:59:31 -0700

    100 first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * print_quote - prints a quote
+ * function is executed
+ */
+void print_quote(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
