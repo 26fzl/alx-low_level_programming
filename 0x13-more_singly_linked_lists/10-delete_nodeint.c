e5cbd033dbfc1d8c367817c1b6f673778d387d23
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-06-05 14:27:56 -0700

    10 delete nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,7 +10,7 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *cur = *head;
-	listint_t *temp = NULL;
+	listint_t *tem = NULL;
 	unsigned int x;
 
 	if (index == 0)
