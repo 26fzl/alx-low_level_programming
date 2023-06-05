4fd1f8360867ccf272b72c7d511cde76c3e16796
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-06-05 14:05:41 -0700

    9 insert nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,7 +33,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (x == idx - 1)
 	{
 		new_nod->next = cur->next;
-		curt->next = new_nod;
+		cur->next = new_nod;
 		return (new_nod);
 	}
 	else
