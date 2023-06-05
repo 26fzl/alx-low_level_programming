63de4df4e38878bb6c1c2337d74f25b940948260
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-06-05 14:49:51 -0700

    10 delete nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,25 +10,29 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int x;
-	listint_t *cur = *head;
-	listint_t *temp = NULL;
+	listint_t *tem = *head;
+	listint_t *cur = NULL;
+
+	if (*head == NULL)
+		return (-1);
 
 	if (index == 0)
 	{
 		*head = (*head)->next;
-		free(cur);
+		free(tem);
 		return (1);
 	}
 
 	for (x = 0;  x < index - 1; x++)
 	{
-		if (!cur || (cur->next))
+		if (!tem || (tem->next))
 			return (-1);
-		cur = cur->next;
+		tem = tem->next;
 	}
 
-	temp = cur->next;
-	cur->next = temp->next;
-	free(temp);
+	cur = tem->next;
+	tem->next = cur->next;
+	free(cur);
+
 	return (1);
 }
