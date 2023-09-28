f036043466a14fe274bab040d98f81f28106a677
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-09-28 03:04:54 -0700

    tasks

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
-""" returns the perimeter of the island described in grid"""
+"""returns the perimeter of the island described in grid"""
 
 def island_perimeter(grid):
     """Return the perimeter of an island.
+    Args:
+        grid (list): A list of list of integers representing an island.
+    Returns:
+        The perimeter of the island defined in grid.
     """
     height, width = len(grid), len(grid[0])
-    edges, size = 0, 0
+    edges = 0, size = 0
 
-    for i in range(height):
-        for j in range(width):
-            if grid[i][j] == 1:
+    for x in range(height):
+        for y in range(width):
+            if grid[x][y] == 1:
                 size += 1
-                if j > 0 and grid[i][j - 1] == 1:
+                if (y > 0 and grid[x][y - 1] == 1):
                     edges += 1
-                if i > 0 and grid[i - 1][j] == 1:
+                if (x > 0 and grid[x - 1][y] == 1):
                     edges += 1
     return size * 4 - edges * 2
+
